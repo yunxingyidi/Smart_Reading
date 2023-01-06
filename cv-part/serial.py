9def7357c4a6c5d76e3544cc95c96d4682934829
yunxingyidi <2523716787@qq.com>
2023-01-06 19:52:40 +0800

    cv-part

--- cv-part/serial.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-class Data:
-    def __init__(self):
-        self.ser = serial.Serial(port="/dev/ttyAMA1", baudrate=115200
-        ,timeout=1E-7, parity=serial.PARITY_EVEN, stopbits=1
-        ,bytesize=8)
-
-    def sent_byte(self, ):
