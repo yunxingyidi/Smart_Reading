21f9d6e2db966d591bc15d597726ac3c17e571e9
yunxingyidi <2523716787@qq.com>
2023-01-06 20:09:38 +0800

    First

---
+++ cv-part/fingercounter.py
@@ -0,0 +1,108 @@
+import cv2
+import HandTrackingModule1 as htm
+import scan as sc
+import volume as vo
+import sound as so
+import threading
+import time
+
+class Fingercounter(threading.Thread):
+    def __init__(self, cap):
+        threading.Thread.__init__(self)
+        self.wCam = 640
+        self.hCam = 480
+        self.cap = cap
+        self.detector = htm.handDetector(detectionCon=0.75)
+
+        self.tipIds = [4, 8, 12, 16, 20]
+        self.last_Finger = 0
+        self.count = 0
+
+
+    def run(self):
+        scan = sc.Scan(self.cap)
+        volume = vo.Volume(self.cap)
+        sound = so.Sound()
+        pTime = 0
+        delay = 0
+        scan_flag = False
+        sign = False
+        bbox = []
+        while True:
+            success, img = self.cap.read()
+            img = cv2.resize(img, (640, 480))
+            img = self.detector.findHands(img)
+            lmList = self.detector.findPosition(img, draw=False)
+
+
+            # print(lmList)
+            if scan_flag:
+                delay += 1
+                if delay <= 50:
+                    continue
+                else:
+                    read_text, sign = scan.handle()
+                    if sign:
+                        print(read_text)
+                    else:
+                        sound.read(read_text)
+                        sound.change_state(True)
+                        sound.start()
+                    self.count = 0
+                    delay = 0
+                    scan_flag = False
+            if len(lmList) != 0:
+                fingers = []
+                # Thumb
+                if lmList[self.tipIds[0]][1] > lmList[self.tipIds[0] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+                # 4 Fingers
+                for id in range(1, 5):
+                    if lmList[self.tipIds[id]][2] < lmList[self.tipIds[id] - 2][2]:
+                        fingers.append(1)
+                    else:
+                        fingers.append(0)
+
+                # print(fingers)
+                totalFingers = fingers.count(1)
+                if totalFingers == self.last_Finger:
+                    self.count += 1
+                else:
+                    self.count = 0
+                if self.count == 10:
+                    if totalFingers == 1:
+                        sign = sound.change_state(sign)
+                        self.count = 0
+                    elif totalFingers == 2:
+                        scan_flag = True
+                        # read_text, sign = scan.handle()
+                        # if sign:
+                        #     print(read_text)
+                        # else:
+                        #     sound.read(read_text)
+                        #     sound.change_state(True)
+                        #     sound.start()
+                        # self.count = 0
+                    elif totalFingers == 3:
+                        volume.handle()
+                        self.count = 0
+                    elif totalFingers == 4:
+                        self.count = 0
+                    else:
+                        self.count = 0
+                self.last_Finger = totalFingers
+                print(totalFingers)
+                cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, str(totalFingers - 1), (45, 375), cv2.FONT_HERSHEY_PLAIN,
+                            10, (255, 0, 0), 25)
+                cTime = time.time()
+                fps = 1 / (cTime - pTime)
+                pTime = cTime
+                cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+                            1, (255, 0, 0), 3)
+
+            cv2.imshow("Image", img)
+            cv2.waitKey(1)
---
+++ cv-part/scan.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+import utlis
+import ocr
+import globalvar as gl
+
+class Scan:
+    def __init__(self, cap):
+        self.cap = cap
+
+    def handle(self):
+        print("In the scan")
+        gl._init()
+        heightImg = 500
+        widthImg = 300
+        success, img = self.cap.read()
+        img = cv2.resize(img, (widthImg, heightImg))  # RESIZE IMAGE
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # CONVERT IMAGE TO GRAY SCALE
+        imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)  # ADD GAUSSIAN BLUR
+        imgThreshold = cv2.Canny(imgBlur, 100, 100)  # APPLY CANNY BLUR
+        kernel = np.ones((5, 5))
+        imgDial = cv2.dilate(imgThreshold, kernel, iterations=2)  # APPLY DILATION
+        imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+
+        #返回边界轮廓的所有点的集合，是一个num，x，y的三维数组
+        contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL,
+                                               cv2.CHAIN_APPROX_SIMPLE)  # FIND ALL CONTOURS
+
+        # FIND THE BIGGEST COUNTOUR
+        biggest = utlis.biggestContour(contours)  # FIND THE BIGGEST CONTOUR
+        if len(biggest) != 0:
+            biggest = utlis.reorder(biggest)
+            pts1 = np.float32(biggest)  # PREPARE POINTS FOR WARP
+            pts2 = np.float32([[0, 0], [widthImg, 0], [0, heightImg], [widthImg, heightImg]])  # PREPARE POINTS FOR WARP
+            matrix = cv2.getPerspectiveTransform(pts1, pts2)
+            imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+            # REMOVE 20 PIXELS FORM EACH SIDE
+            imgWarpColored = imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+            imgWarpColored = cv2.resize(imgWarpColored, (widthImg, heightImg))
+
+            read_text =ocr.OCR_demo(imgWarpColored)
+
+            return read_text, True
+        else:
+            return "Can't find the picture", False
+        # heightImg = 500
+        # widthImg = 300
+        # success, img = self.cap.read()
+        # img = cv2.resize(img, (widthImg, heightImg))
+        #
+        # read_text = ocr.OCR_demo(img)
+        # return read_text, True
+
